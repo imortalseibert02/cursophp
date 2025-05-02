27c3381997c17bc336b3141b5f17894547be24d9
imortalseibert02 <Felipe.Seibert.2003@gmail.com>
2025-05-02 15:35:22 -0300

    php xamp

---
+++ ex004/cad.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+
+<body>
+    <header>
+        <h1>Resultado processamneto</h1>
+    </header>
+    <main>
+        <?php
+       
+
+<p><a href="javascript:history.go(-1)">Voltar para a pagina anterior</a></p>
+
+    </main>
+
+
+
+</body>
+
+</html>
